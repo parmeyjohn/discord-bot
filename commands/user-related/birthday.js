8d096dd18cbc3ceebe4ce30f7cdb96d7f70bc9ff
Nick Cuviello <nickcuv@gmail.com>
2024-04-03 23:07:04 -0700

    added date checking on birthdate input

--- commands/user-related/birthday.js
+++ commands/user-related/birthday.js
@@ -17,34 +17,19 @@ const data = new SlashCommandBuilder()
 
 const execute = async (interaction) => {
   const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    ["January", 31],
+    ["February", 29],
+    ["March", 31],
+    ["April", 30],
+    ["May", 31],
+    ["June", 30],
+    ["July", 31],
+    ["August", 31],
+    ["September", 30],
+    ["October", 31],
+    ["November", 30],
+    ["December", 31],
   ];
-
-  const days = {
-    January: 31,
-    February: 29,
-    March: 31,
-    April: 30,
-    May: 31,
-    June: 30,
-    July: 31,
-    August: 31,
-    September: 30,
-    October: 31,
-    November: 30,
-    December: 31,
-  };
 
   const modal = new ModalBuilder({
     customId: `bdayModal-${interaction.user.id}`,
@@ -78,12 +63,27 @@ const execute = async (interaction) => {
 
   const filter = (i) => i.customId === `bdayModal-${i.user.id}`;
 
-  interaction.awaitModalSubmit({ filter, time: 30_000 }).then((i) => {
+  interaction.awaitModalSubmit({ filter, time: 300_000 }).then((i) => {
     const month = i.fields.getTextInputValue("monthInput");
     const day = i.fields.getTextInputValue("dayInput");
+    const intMonth = parseInt(month);
+    const intDay = parseInt(day);
+    if (
+      isNaN(intMonth) ||
+      isNaN(intDay) ||
+      intMonth < 1 ||
+      intMonth > 12 ||
+      intDay < 1 ||
+      intDay > months[intMonth - 1][1]
+    ) {
+      return i.reply({
+        content: `Please input valid numeric dates for month and day`,
+        ephemeral: true,
+      });
+    }
 
     i.reply({
-      content: `You set your birthday to ${month} ${day}`,
+      content: `You set your birthday to ${months[intMonth - 1][0]} ${day}`,
       ephemeral: true,
     });
   });
